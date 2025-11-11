ce0b1539eff6bcc36e8150245d46f179dfd91121
dsbnd <dashabond07@mail.ru>
2025-11-11 16:43:06 +0300

    conflict

--- src/main/java/lab1/ArtishokApplication.java
+++ src/main/java/lab1/ArtishokApplication.java
@@ -8,8 +8,11 @@ public class ArtishokApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello suchka");
+
 		System.out.println("Делаю изменения, сучка!");
 		// привет всем!
+		System.out.println("я первая сучка");
+
 		SpringApplication.run(ArtishokApplication.class, args);
 	}
 
