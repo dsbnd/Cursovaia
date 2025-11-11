0ed246736a882ea565e046ebb8af28e7ecfecd9a
AsyaKhar <409792@edu.itmo.ru>
2025-11-11 16:52:05 +0300

    revert

--- src/main/java/lab1/ArtishokApplication.java
+++ src/main/java/lab1/ArtishokApplication.java
@@ -8,10 +8,11 @@ public class ArtishokApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello suchka");
-
 		System.out.println("Делаю изменения, сучка!");
 		// привет всем!
+
 		System.out.println("я первая сучк");
+
 
 		SpringApplication.run(ArtishokApplication.class, args);
 	}
