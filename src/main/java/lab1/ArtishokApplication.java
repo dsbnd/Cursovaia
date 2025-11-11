3359fd0e3d9c1cb6d36d5658ae42df88c516e9ac
dsbnd <dashabond07@mail.ru>
2025-11-11 16:24:08 +0300

    first1

--- src/main/java/lab1/ArtishokApplication.java
+++ src/main/java/lab1/ArtishokApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ArtishokApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello suchka");
 		SpringApplication.run(ArtishokApplication.class, args);
 	}
 
